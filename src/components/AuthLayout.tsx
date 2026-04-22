150ec6ea118d05551d422662994d21d4d4e04528
TheJimmyJam <wcannon83@gmail.com>
2026-04-21 21:00:36 -0500

    theme: dark premium - AuthLayout

--- src/components/AuthLayout.tsx
+++ src/components/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Navbar } from './Navbar'
 
 export function AuthLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col">
+    <div className="min-h-screen bg-[#0f0f1a] flex flex-col">
       <Navbar />
       <div className="flex-1">{children}</div>
     </div>
